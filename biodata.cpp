b4e9dff4931b6af8e6480453af92052b8c717f64
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-14 12:20:32 +0600

    c++ variable, int, double, float, char

---
+++ biodata.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    // printing my name 
+    /* multiple line
+    comment
+    */
+    cout << "Mainul Islam Faruqi" << endl;
+    // printing my work definition 
+    cout << "first running cpp programm";
+    return 0;
+}
---
+++ program2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+// #include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+
+    num1 = 10;
+    num2 = 20;
+
+    double num3 = 33.33;
+    float num4 = 33.3333333333333;
+
+    char ch = 'b';
+
+    cout << "Number-1 = " << num1 <<endl;
+    cout << "Number-2 = " << num2 <<endl;
+    cout << "Number-3 = " << num3 <<endl;
+    cout << "Number-4 = " << num4 <<endl;
+    cout << ch <<endl;
+    // getch();
+    return 0;
+
+}
