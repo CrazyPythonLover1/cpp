3a118d54eaf08c07a272d9c6fc754ace9f248104
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-17 21:44:55 +0600

    char as a Array

---
+++ program3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Mainul Islam";
+    // char name[13] = "Mainul Islam";
+    cout << name <<endl;
+    return 0;
+}
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+    
+//     return 0;
+// }
