96727b81da83795018d172282ac443f379495afc
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-22 09:05:51 +0600

    logical operator

---
+++ Operators/logical_operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter any letter ";
+    cin >> ch;
+
+    if (ch == 'a' || ch == 'e' || ch=='i' || ch == 'o' || ch == 'u') {
+        cout << "Vowel" << endl;
+    } else {
+        cout << "Consonant" << endl;
+    }
+    
+    return 0;
+}
