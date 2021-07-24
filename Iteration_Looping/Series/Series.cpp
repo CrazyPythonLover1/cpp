1960b1a1d91ffeea5dd64c4faebd242174e8634e
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-07-24 20:28:59 +0600

    even, odd series

--- Iteration_Looping/Series/Series.cpp
+++ Iteration_Looping/Series/Series.cpp
@@ -10,9 +10,24 @@ int main()
 
     for (int i = 1; i<=n; i++)
     {
+        // sum += i;
+        // cout << i << endl;
+    }
+
+    // Even Series
+    for (int i = 2; i <=n; i+=2)
+    {
+        // sum += i;
+        // cout << i << endl;
+    }
+
+    // Odd Series 
+    for( int i = 1; i<=n; i+=2)
+    {
         sum += i;
         cout << i << endl;
     }
+    
     cout << sum;
 
     return 0;
