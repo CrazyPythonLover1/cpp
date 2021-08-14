50c31cd1d74ccbade07fafcdf2f75cbc86accc43
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-08-14 15:45:26 +0600

    sum with pointer

--- Pointer.cpp
+++ Pointer.cpp
@@ -4,15 +4,27 @@ using namespace std;
 
 int main()
 {
-    int x = 5;
-    int *p;
+    // int x = 5;
+    // int *p;
 
-    p = &x;
+    // p = &x;
 
-    cout << x << endl;
-    cout << &x << endl;
-    cout << p << endl;
-    cout << *p << endl;
+    // cout << x << endl;
+    // cout << &x << endl;
+    // cout << p << endl;
+    // cout << *p << endl;
+
+    int num1 = 10;
+    int num2 = 20;
+
+    int *p1, *p2;
+
+    p1 = &num1;
+    p2 = &num2;
+
+    int sum = *p1 + *p2;
+
+    cout << sum << endl;
 
 
     return 0;
